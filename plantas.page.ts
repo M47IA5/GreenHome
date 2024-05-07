57db352b105c0ba251e24701d4bce7961967b1f4
Matias Mella <106762753+M47IA5@users.noreply.github.com>
2024-05-07 10:10:09 -0400

    Add files via upload

---
+++ plantas.page.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+
+register(); 
+@Component({
+  selector: 'app-plantas',
+  templateUrl: './plantas.page.html',
+  styleUrls: ['./plantas.page.scss'],
+})
+export class PlantasPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+}
